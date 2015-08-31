934d2546112a8a5f7470331e99442497926054a5
ElectroORG <adam@adamcomp.com>
2015-08-31 07:42:03 +0200

    files

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/style.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="UTF-8" />
+		<title>Responsive</title>
+	</head>
+	<body>
+		<header>
+			<div class="logo">Responsive</div>
+			<nav class="menu">
+				<ul class="menu">
+					<a href="#"><li>Home</li></a>
+					<a href="#"><li>Products</li></a>
+					<a href="#"><li>About</li></a>
+					<a href="#"><li>Offer</li></a>
+					<a href="#"><li>Contact</li></a>
+				</ul>
+			</nav>
+		</header>
+		<div class="content">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</body>
+</html>
